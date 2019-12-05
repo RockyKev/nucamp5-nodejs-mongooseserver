13dcb0309d9b5994da8a6f72296d38a514cc85e8
Rocky Kev <kev.rocky@gmail.com>
2019-12-04 19:40:31 -0800

    mongoose part 3

--- index.js
+++ index.js
@@ -6,23 +6,48 @@ const url = "mongodb://localhost:27017/conFusion";
 const connect = mongoose.connect(url);
 
 connect.then(db => {
-  console.log("Connected correctly to server");
+  console.log("---> Connected correctly to server");
 
   Dishes.create({
     name: "Uthapizza",
     description: "test"
   })
     .then(dish => {
+      console.log("---> successfully completed");
+
       console.log(dish);
 
-      return Dishes.find({});
+      console.log("---> returning find command");
+      return Dishes.findByIdAndUpdate(
+        dish._id,
+        {
+          $set: { description: "Updated test" }
+        },
+        { new: true }
+      );
     })
-    .then(dishes => {
-      console.log(dishes);
+    .then(dish => {
+      console.log(dish);
 
+      console.log("---> pushing comment into array");
+
+      dish.comments.push({
+        rating: 5,
+        comment: "I'm getting a sinking feeling!",
+        author: "Leonardo di Carpaccio"
+      });
+
+      return dish.save();
+    })
+    .then(dish => {
+      console.log(dish);
+
+      console.log("---> returning remove command");
       return Dishes.remove({});
     })
     .then(() => {
+      console.log("---> returning mongoose close command");
+
       return mongoose.connection.close();
     })
     .catch(err => {
